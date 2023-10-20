12768643fdae7f912b46ded1af13d658df711ab0
Joe Hagan <jhaganfilm@gmail.com>
2023-10-20 18:40:16 +0900

    initial commit. folder organization

---
+++ components/TodoContainer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Todo Container</h2>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <TodoContainer />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TodoContainer from "../components/TodoContainer.vue"
+</script>
+
+<style></style>
